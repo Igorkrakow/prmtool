1033d9cb13ef4f71dd97ae957ec308d21d70697a
tjx74946 <mykola.storozhenko@igt.com>
2023-07-11 15:45:23 +0300

    - add Stored Procedure to insert data in MIGRATED_TX_DRAW_ENTRY - add indexes to TXSTORE.LOTTERY_TX_HEADER table

--- plt-csv-exporter/TXSTORE_creation.sh
+++ plt-csv-exporter/TXSTORE_creation.sh
@@ -98,6 +98,80 @@ db2 " $start_index
       CREATE INDEX TXSTORE.IDXMIGRATED_TX_DRAW_ENTRY ON TXSTORE.MIGRATED_TX_DRAW_ENTRY ( UUID ASC )
       $end"
 
+db2 " $start_index
+      CREATE index TXSTORE.LOTTERY_TX_HEADER_GLOBAL_TRANS_ID_START_DRAW_NUMBER
+          on TXSTORE.LOTTERY_TX_HEADER (GLOBAL_TRANS_ID,START_DRAW_NUMBER,LOTTERY_TRANSACTION_TYPE)
+      $end"
+
+db2 "REORG INDEXES ALL FOR TABLE TXSTORE.LOTTERY_TX_HEADER"
+
+db2 "CREATE OR REPLACE PROCEDURE TXSTORE.INSERT_INTO_MIGRATED_TX_DRAW_ENTRY()
+         DYNAMIC RESULT SETS 1
+         LANGUAGE SQL
+     BEGIN
+         -- Declare variables
+         declare SQLCODE int default 0;
+         DECLARE V_SQLCODE INT DEFAULT 0;
+         DECLARE v_LOTTERY_TX_HEADER_ID BIGINT;
+         DECLARE v_START_DRAW_NUMBER INT;
+         DECLARE v_END_DRAW_NUMBER INT;
+         DECLARE v_DRAW_NUMBER INT;
+         DECLARE v_PRODUCT INT;
+         DECLARE v_UUID VARCHAR(200);
+         DECLARE v_GLOBAL_TRANS_ID VARCHAR(50);
+         DECLARE V_COUNT_VALIDATION INT;
+         -- Declare cursor
+         DECLARE LOTTERY_TX_HEADER_FOR_ECH_WAGER CURSOR WITH HOLD FOR
+             SELECT
+                 D.LOTTERY_TX_HEADER_ID,
+                 D.START_DRAW_NUMBER,
+                 D.END_DRAW_NUMBER,
+                 D.PRODUCT,
+                 H.UUID,
+                 D.GLOBAL_TRANS_ID
+             FROM
+                 TXSTORE.LOTTERY_TX_HEADER D
+                 INNER JOIN TXSTORE.MIGR_TX_HEADER H
+                     ON D.LOTTERY_TX_HEADER_ID = H.TX_HEADER_ID
+             WHERE   D.LOTTERY_TRANSACTION_TYPE = 'WAGER';
+         DECLARE CONTINUE HANDLER FOR SQLEXCEPTION, NOT FOUND, SQLWARNING
+             SET V_SQLCODE = SQLCODE;
+         -- Open cursor
+         OPEN LOTTERY_TX_HEADER_FOR_ECH_WAGER;
+
+         FETCH LOTTERY_TX_HEADER_FOR_ECH_WAGER INTO v_LOTTERY_TX_HEADER_ID,
+             v_START_DRAW_NUMBER, v_END_DRAW_NUMBER,v_PRODUCT,v_UUID,v_GLOBAL_TRANS_ID;
+
+         WHILE (V_SQLCODE = 0) DO
+                 SET v_DRAW_NUMBER = v_START_DRAW_NUMBER;
+                 WHILE (v_DRAW_NUMBER <= v_END_DRAW_NUMBER) DO
+                         SELECT  COUNT(*)
+                         INTO    V_COUNT_VALIDATION
+                         FROM    TXSTORE.LOTTERY_TX_HEADER
+                         WHERE   LOTTERY_TRANSACTION_TYPE = 'VALIDATION'
+                                 and GLOBAL_TRANS_ID=v_GLOBAL_TRANS_ID
+                                 and START_DRAW_NUMBER=v_DRAW_NUMBER;
+
+                         INSERT INTO TXSTORE.MIGRATED_TX_DRAW_ENTRY (ID,UUID,DRAWNUMBER,PRODUCT,WIN_STATUS)
+                         VALUES (
+                                    NEXT VALUE FOR TXSTORE.MIGRATED_TX_DRAW_ENTRY_SEQ,
+                                    v_UUID,
+                                    v_DRAW_NUMBER,
+                                    v_PRODUCT,
+                                    CASE WHEN V_COUNT_VALIDATION >= 1 THEN 'WINNING' ELSE 'NON_WINNING' END
+                                );
+                         commit;
+                     SET v_DRAW_NUMBER = v_DRAW_NUMBER + 1;
+                 END WHILE;
+                 SET V_SQLCODE = 0;
+                 FETCH LOTTERY_TX_HEADER_FOR_ECH_WAGER INTO v_LOTTERY_TX_HEADER_ID,
+                             v_START_DRAW_NUMBER, v_END_DRAW_NUMBER,v_PRODUCT,v_UUID,v_GLOBAL_TRANS_ID;
+                 IF (V_SQLCODE <> 0) THEN
+                             CALL SYSIBMADM.DBMS_OUTPUT.PUT_LINE('End reading cursor');
+                 END IF;
+         END WHILE;
+     CLOSE LOTTERY_TX_HEADER_FOR_ECH_WAGER;
+     END"
 ##########    Create VIEW  MIGRATED_TX_DRAW    ##################
 
 db2 "CREATE OR REPLACE VIEW TXSTORE.MIGRATED_TX_DRAW AS
