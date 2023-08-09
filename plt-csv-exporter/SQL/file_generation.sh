90f6463d1795b6febd7df438a0f03256f01387d8
tjx74946 <mykola.storozhenko@igt.com>
2023-08-09 18:35:07 +0300

    - add draw and draw entry generation

--- plt-csv-exporter/SQL/file_generation.sh
+++ plt-csv-exporter/SQL/file_generation.sh
@@ -132,3 +132,105 @@ for f in $csvFileName'_unix-'*
               rm $f
       done
 rm $csvFileName'_HEADER.csv'
+
+echo "---------------------------"
+echo "TX-DRAW"
+echo "---------------------------"
+db2 connect to pddb
+log_with_timestamp "CREATE tx-draws_ files"
+db2 export to tx-draws_HEADER.csv OF DEL MODIFIED BY NOCHARDEL  "
+            SELECT
+                'product_id',
+                'draw_id',
+                'draw_name',
+                'draw_time',
+                'draw_status'
+            FROM sysibm.sysdummy1"
+db2 export to tx-draws_TMP.csv OF DEL MODIFIED BY NOCHARDEL  "
+            SELECT
+                E.IDDGGAME as product_id,
+                E.DRAWNUMBER as draw_id,
+                null as draw_name,
+                varchar_format(E.DRAWDATE,'YYYY-MM-DD HH24:MI:SS.FF3') as draw_time,
+                case when g.NAME is not null then
+                    'ACTIVE'
+                else 'CLOSED' end as draw_status
+            FROM GIS.DGGAMEEVENT E
+            left join GIS.DGGAME G ON G.IDDGGAME=E.IDDGGAME
+            AND G.IDDGGAMEEVENT_CURRENT=E.IDDGGAMEEVENT"
+###########################
+db2 terminate
+###########################
+###########################
+csvFileName="tx-draws"
+###########################
+split --numeric-suffixes --suffix-length=3  -l $num_rows $csvFileName'_TMP.csv' $csvFileName'_unix-'$fileNameEndDate'_'
+for file in tx-draws_unix-*
+do
+mv "$file" "$file.csv"
+done
+count=$(wc -l < $csvFileName'_TMP.csv')
+if (($count==0)); then
+  mv $csvFileName'_TMP.csv' $csvFileName'_unix-'$fileNameEndDate'_001.csv'
+else
+  rm $csvFileName'_TMP.csv'
+fi
+for f in $csvFileName'_unix-'*
+      do
+              head -n 1 $csvFileName'_HEADER.csv' > $csvFileName'_HEADER_TMP.csv'
+              cat "$f" >>$csvFileName'_HEADER_TMP.csv'
+              mv -f $csvFileName'_HEADER_TMP.csv' "$f"
+              perl -p -e 's/\n/\r\n/' < $f > ${f//_unix/}
+              rm $f
+      done
+rm $csvFileName'_HEADER.csv'
+
+echo "---------------------------"
+echo "TX-DRAW-ENTRY"
+echo "---------------------------"
+db2 connect to pddb
+log_with_timestamp "CREATE tx-draw-entry_ files"
+db2 export to tx-draw-entry_HEADER.csv OF DEL MODIFIED BY NOCHARDEL  "
+            SELECT
+               'tx_draw_entry_id',
+               'tx_transaction_uuid',
+               'draw_id',
+               'product_id',
+               'winning_status',
+               'json_data'
+            FROM sysibm.sysdummy1"
+db2 export to tx-draw-entry_TMP.csv OF DEL MODIFIED BY NOCHARDEL  "
+            SELECT
+                DE.id as tx_draw_entry_id,
+                DE.uuid as tx_transaction_uuid,
+                DE.drawnumber as draw_id,
+                DE.product as product_id,
+                DE.win_status as winning_status,
+                null as json_data
+            FROM TXSTORE.MIGRATED_TX_DRAW_ENTRY as DE"
+###########################
+db2 terminate
+###########################
+###########################
+csvFileName="tx-draw-entry"
+###########################
+split --numeric-suffixes --suffix-length=3  -l $num_rows $csvFileName'_TMP.csv' $csvFileName'_unix-'$fileNameEndDate'_'
+for file in tx-draw-entry_unix-*
+do
+mv "$file" "$file.csv"
+done
+count=$(wc -l < $csvFileName'_TMP.csv')
+if (($count==0)); then
+  mv $csvFileName'_TMP.csv' $csvFileName'_unix-'$fileNameEndDate'_001.csv'
+else
+  rm $csvFileName'_TMP.csv'
+fi
+for f in $csvFileName'_unix-'*
+      do
+              head -n 1 $csvFileName'_HEADER.csv' > $csvFileName'_HEADER_TMP.csv'
+              cat "$f" >>$csvFileName'_HEADER_TMP.csv'
+              mv -f $csvFileName'_HEADER_TMP.csv' "$f"
+              perl -p -e 's/\n/\r\n/' < $f > ${f//_unix/}
+              rm $f
+      done
+rm $csvFileName'_HEADER.csv'
